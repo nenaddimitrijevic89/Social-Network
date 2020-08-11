5977940a9c1e9056f7c007ef4d7823bd49c7c951
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2020-08-11 14:14:28 -0700

    header and footer created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize';
+import Header from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
 
 function App() {
   return (
-    <div>
-      
+    <div className='page-container'>
+      <div className='content'>
+      <Header/>
+      </div>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Navbar, NavItem} from 'react-materialize';
+import style from './Header.module.css';
+
+const Header =()=>{
+    return(
+        <Navbar
+  alignLinks="right"
+  brand={<a className="brand-logo" href="#">Social Network</a>}
+  centerChildren
+  id="mobile-nav"
+  menuIcon={<><div className={style.menu}></div><div className={style.menu}></div><div className={style.menu}></div></>}
+  options={{
+    draggable: true,
+    edge: 'left',
+    inDuration: 250,
+    onCloseEnd: null,
+    onCloseStart: null,
+    onOpenEnd: null,
+    onOpenStart: null,
+    outDuration: 200,
+    preventScrolling: true
+  }}
+>
+  <NavItem href="">
+    Feed
+  </NavItem>
+  <NavItem href="">
+    People
+  </NavItem>
+  <NavItem href="">
+    Profile
+  </NavItem>
+</Navbar>
+    )
+}
+
+export default Header;
