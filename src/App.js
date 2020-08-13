e162108f365cdb96922a4db77ef3c5e5a4f9179e
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2020-08-13 13:10:44 -0700

    router implementation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize';
 import Header from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
