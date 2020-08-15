317b92f76c9d7978093d4996d4faf399db610a74
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2020-08-15 22:41:16 +0200

    hardcoded text post created

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import 'materialize-css/dist/js/materialize';
 import Header from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
 import {Switch, Route} from 'react-router-dom';
+import { Feed } from './components/Feed/Feed';
 
 function App() {
   return (
     <div className='page-container'>
       <div className='content'>
       <Header/>
+      <Switch>
+        <Route path='/feed' component={Feed}/>
+      </Switch>
       </div>
       <Footer/>
     </div>
---
+++ src/components/Feed/TextPost.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Row, Col, Card, Icon} from 'react-materialize';
+
+const TextPost=()=>{
+    return(
+        <Row>
+  <Col
+    m={12}
+    s={12}
+  >
+    <Card
+      actions={[
+        <a key="1" href="#">Text Post</a>,
+        <a key="2" href="#">Comments</a>
+      ]}
+      className="blue-grey darken-1"
+      closeIcon={<Icon>close</Icon>}
+      revealIcon={<Icon>more_vert</Icon>}
+      textClassName="white-text"
+      title="Card title"
+    >
+      Post
+    </Card>
+  </Col>
+</Row>
+    )
+}
+
+export {TextPost};
