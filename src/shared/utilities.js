e90f16933a560a1bc59091c6663279a8c09dc78c
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-09-06 20:58:17 +0200

    changing post components

--- src/shared/utilities.js
+++ src/shared/utilities.js
@@ -3,9 +3,9 @@ const { storageService } = require("../services/storageService")
 const getAuthHeader = () => {
     const token = storageService.get('token')
     if (token) {
-            return `Bearer ${token}`
+        return `Bearer ${token}`
     }
     return 'Bearer sn-token'
 }
 
-export {getAuthHeader}
+export { getAuthHeader }
