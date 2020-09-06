d08c05fdb73e11f2b3a34a8858fc4d3b18820c6a
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-09-06 19:18:07 +0200

    user component created

--- src/view/components/People/SingleUser/SingleUser.js
+++ src/view/components/People/SingleUser/SingleUser.js
@@ -1,37 +1,36 @@
 import React from 'react';
 import { userService } from '../../../../services/userService';
 import { Button } from 'react-materialize';
-import { storageService } from '../../../../services/storageService';
 
-class SingleUser extends React.Component{
-    constructor(props){
+class SingleUser extends React.Component {
+    constructor(props) {
         super(props)
-        this.state={
+        this.state = {
             user: null,
             posts: []
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         userService.getSingleUser(this.props.match.params.id)
-        .then(response=>this.setState({ user: response }))
+            .then(response => this.setState({ user: response }))
         userService.getSingleUserPosts(this.props.match.params.id)
     }
 
-    removeUser=()=>{
+    removeUser = () => {
         userService.deleteSingleUser(this.props.match.params.id, this.state.user)
-        .then(()=>{
-            this.props.history.push('/')
-        })
+            .then(() => {
+                this.props.history.push('/')
+            })
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <>
-            <Button
-            onClick={this.removeUser}
-            >Remove User
-            </Button>
+                <Button
+                    onClick={this.removeUser}
+                >Remove User
+                </Button>
             </>
         )
     }
--- src/view/components/People/User/User.js
+++ src/view/components/People/User/User.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Col } from 'react-materialize';
+import { Col, Row, Card, CardTitle } from 'react-materialize';
 import { Link } from 'react-router-dom';
 
-const User=({ user })=>{
-    const {firstName, lastName, email, id} = user;
-    return(
-        <Link to={`/people/${id}`}><Col>
-            <h4>{`${firstName} ${lastName}`}</h4>
-            <h6>{email}</h6>
-        </Col></Link>
+const User = ({ user }) => {
+    const { firstName, lastName, email, id } = user;
+    return (
+        <Link to={`/people/${id}`}>
+            <Row>
+                {/* <Col s={12} m={6} l={4}>
+                    <Card
+                        header={<CardTitle></CardTitle>}
+                    ></Card>
+                </Col> */}
+                <Col s={12} m={6} l={8}>
+                    <Card className=''>
+                        <h3 className=''>{`${firstName} ${lastName}`}</h3>
+                        <h5>{email}</h5>
+                    </Card>
+                </Col>
+            </Row>
+        </Link>
     )
 }
 
