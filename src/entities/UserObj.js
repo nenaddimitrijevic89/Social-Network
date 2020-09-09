dea907f0d2fd1e49b1891b575a724c1034617d88
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-09-09 20:47:30 +0200

    added patch method

--- src/entities/UserObj.js
+++ src/entities/UserObj.js
@@ -1,12 +1,13 @@
-class User{
-    constructor(user){
-        this.id=user._id;
-        this.avatarUrl=user.avatarUrl;
-        this.about=user.about;
-        this.firstName=user.firstName;
-        this.lastName=user.lastName;
-        this.fullName=user.fullName;
-        this.email=user.email;
+class User {
+    constructor(user) {
+        this.id = user._id;
+        this.avatarUrl = user.avatarUrl;
+        this.about = user.about;
+        this.firstName = user.firstName;
+        this.lastName = user.lastName;
+        this.fullName = user.fullName;
+        this.email = user.email;
+        this.prefix = user.prefix;
     }
 }
 
