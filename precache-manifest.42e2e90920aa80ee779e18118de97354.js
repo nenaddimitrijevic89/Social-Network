50abdbf5d0eeeca78a99e9db58b59eb76b1980b2
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2020-10-12 21:56:00 +0200

    Updates

---
+++ precache-manifest.42e2e90920aa80ee779e18118de97354.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b5ce71de6ea1455e4de1671e26674a",
+    "url": "/Social-Network/index.html"
+  },
+  {
+    "revision": "1b6bd11d3c3d571dbe2c",
+    "url": "/Social-Network/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "2c294cf069ec95805dc2",
+    "url": "/Social-Network/static/css/main.68cc4d45.chunk.css"
+  },
+  {
+    "revision": "1b6bd11d3c3d571dbe2c",
+    "url": "/Social-Network/static/js/2.4158ece2.chunk.js"
+  },
+  {
+    "revision": "0cc6ec6fb09a38105008c4a3cec37be7",
+    "url": "/Social-Network/static/js/2.4158ece2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c294cf069ec95805dc2",
+    "url": "/Social-Network/static/js/main.f311c99e.chunk.js"
+  },
+  {
+    "revision": "f2bc9566228d57cc4ed9",
+    "url": "/Social-Network/static/js/runtime-main.eee90ab2.js"
+  },
+  {
+    "revision": "90f7e49c4a86b04c2b2c5389d49f91bc",
+    "url": "/Social-Network/static/media/user.90f7e49c.png"
+  }
+]);
