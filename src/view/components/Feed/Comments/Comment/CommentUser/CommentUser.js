214260eedacd9ff0222f915ce647b14f966c26f2
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2020-10-17 22:10:58 +0200

    implemnting user link

--- src/view/components/Feed/Comments/Comment/CommentUser/CommentUser.js
+++ src/view/components/Feed/Comments/Comment/CommentUser/CommentUser.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { convertDate } from '../../../../../../shared/utilities';
 import style from './CommentUser.module.css';
 import avatar from '../../../../../../images/user.png';
+import { Link } from 'react-router-dom';
 
 const CommentUser =({ comment, commentUser, deleteComment })=>{
     return(
         <div className={style.user}>
-            <div className={style.img}>
-                <img className={style.radius} src={commentUser?.avatarUrl || avatar} alt='user'></img>
-            </div>
+            <Link to={`/people/${commentUser.id}`}>
+                <div className={style.img}>
+                    <img className={style.radius} src={commentUser?.avatarUrl || avatar} alt='user'></img>
+                </div>
+            </Link>
             <div className={style.info}>
                 <p><span className={style.bold}> {commentUser?.fullName || 'Deleted Account'}</span></p>
                 <p><span> {convertDate(comment.createdAt)}</span></p>
--- src/view/components/Feed/PostUser/PostUser.js
+++ src/view/components/Feed/PostUser/PostUser.js
@@ -2,15 +2,18 @@ import React from 'react';
 import style from './PostUser.module.css';
 import avatar from '../../../../images/user.png';
 import { convertDate } from '../../../../shared/utilities';
+import { Link } from 'react-router-dom';
 
 const PostUser = ({ user, post, deletePost, isShown }) => {
     return (
         <div className={style.user}>
-            <div className={style.img}>
-                {isShown
-                ? <img className={style.radius} src={user?.avatarUrl || avatar} alt='user'></img>
-                : <img className={style.radius} src={user?.avatarUrl || avatar} alt='user'></img>}
-            </div>
+            <Link to={`/people/${user.id}`}>
+                <div className={style.img}>
+                    {isShown
+                    ? <img className={style.radius} src={user?.avatarUrl || avatar} alt='user'></img>
+                    : <img className={style.radius} src={user?.avatarUrl || avatar} alt='user'></img>}
+                </div>
+            </Link>
             <div className={style.info}>
                 {isShown
                 ? <p><span className={style.bold}> {user?.fullName || 'Deleted Account'}</span></p>
