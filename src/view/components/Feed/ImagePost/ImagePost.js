b3d2d19f7db55580304d3597e448b8ade53cc1c1
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2020-10-18 22:02:31 +0200

    fixing bugs

--- src/view/components/Feed/ImagePost/ImagePost.js
+++ src/view/components/Feed/ImagePost/ImagePost.js
@@ -11,7 +11,7 @@ const ImagePost = ({ post, user, deletePost, isShown, comments, users, writeComm
       <Col m={12} s={12} l={9} className={style.margin}>
         <div className={style.card}>
           <PostUser user={user} post={post} deletePost={deletePost} isShown={isShown} numbOfComments={numbOfComments}/>
-          <img src={post.src} alt='imagePost' style={{ width: '100%' }} />
+          <img src={post?.src} alt='imagePost' style={{ width: '100%' }} />
           <Comments isShown={isShown} comments={comments} users={users} writeComment={writeComment} saveComment={saveComment} deleteComment={deleteComment}/>
           {!isShown && <Link to={`/feed/post/${post.id}`}>
                             <i className={`${style.comment} fa fa-comment`}>
