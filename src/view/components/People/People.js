a03a993eeed635f4db1e78cb575f4197e07228e3
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2021-05-08 23:08:42 +0200

    adding auth

--- src/view/components/People/People.js
+++ src/view/components/People/People.js
@@ -6,33 +6,30 @@ import { isLoggedIn } from "../../../shared/utilities";
 import { Loader } from "../Loader/Loader";
 import { postService } from "../../../services/postService";
 import { commentService } from "../../../services/commentService";
+import { useHistory } from "react-router";
 
 const People = () => {
-      const [users, setUsers] = useState([]);
-      const [posts, setPosts] = useState([]);
-      const [comments, setComments] = useState([]);
-      const [isLoading, setIsLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  let history = useHistory();
 
-    useEffect(() => {
+  useEffect(() => {
+    const isAuthorized = isLoggedIn();
+    if (!isAuthorized) {
+      history.push("/");
+    }
 
-    // const isAuthorized = isLoggedIn();
-    // if (!isAuthorized) {
-    //   this.props.history.push("/");
-    // }
+    userService.getAllUsers().then((response) => setUsers(response));
 
-    userService
-      .getAllUsers()
-      .then((response) => setUsers(response));
-
-    postService
-      .getAllPosts()
-      .then((response) => setPosts(response));
+    postService.getAllPosts().then((response) => setPosts(response));
 
     commentService
       .getAllComments()
       .then((response) => setComments(response))
       .finally(() => setIsLoading(false));
-  }, [])
+  }, []);
 
   const filterPostOwner = (id) => {
     const postOwner = posts.filter((post) => post.owner === id);
@@ -40,30 +37,28 @@ const People = () => {
   };
 
   const filterCommentOwner = (id) => {
-    const commentOwner = comments.filter(
-      (comment) => comment.owner === id
-    );
+    const commentOwner = comments.filter((comment) => comment.owner === id);
     return commentOwner.length;
   };
 
-    return (
-      <Container>
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <>
-            {users.map((user) => (
-              <User
-                user={user}
-                key={user.id}
-                numbOfPosts={filterPostOwner(user.id)}
-                numbOfComments={filterCommentOwner(user.id)}
-              />
-            ))}
-          </>
-        )}
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          {users.map((user) => (
+            <User
+              user={user}
+              key={user.id}
+              numbOfPosts={filterPostOwner(user.id)}
+              numbOfComments={filterCommentOwner(user.id)}
+            />
+          ))}
+        </>
+      )}
+    </Container>
+  );
+};
 
 export { People };
