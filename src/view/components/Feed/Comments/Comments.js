2266e403be1c73f5cec3f1e5b19a6591eacd2ecf
Nenad <nenaddimitrijevic89@gmail.com>
2021-06-16 20:21:10 +0200

    Format code style

--- src/view/components/Feed/Comments/Comments.js
+++ src/view/components/Feed/Comments/Comments.js
@@ -1,39 +1,54 @@
-import React from 'react';
-import { Button } from 'react-materialize';
-import { Comment } from './Comment/Comment';
+import React from "react";
+import { Button } from "react-materialize";
+import { Comment } from "./Comment/Comment";
 
-const Comments = ({ isShown, comments, users, writeComment, saveComment, deleteComment }) => {
+const Comments = ({
+  isShown,
+  comments,
+  users,
+  writeComment,
+  saveComment,
+  deleteComment,
+}) => {
+  const writing = (event) => {
+    const text = event.target.value;
+    writeComment(text);
+  };
 
-    const writing = (event) => {
-        const text = event.target.value;
-        writeComment(text)
-    }
-
-    return(
+  return (
+    <>
+      {isShown && (
         <>
-            {isShown
-            &&
-            <>      
-                <div style={{ padding: '10px 24px 20px 24px' }} onKeyUp={event => event.keyCode === 13 && saveComment()}>
-                    <i className='fa fa-pencil'></i> Write comment
-                    <input type='text'onChange={writing}/>
-                    <Button onClick={saveComment}><i className='fa fa-send'></i></Button>
-                </div>
-                <>
-                {comments.length
-                ?<>{comments.map(comment => <Comment
-                                                key={comment.id}
-                                                comment={comment}
-                                                users={users}
-                                                deleteComment={deleteComment}/>)
-                    }</>
-                :<></>
-                }
-                </>
-            </>
-            }
+          <div
+            style={{ padding: "10px 24px 20px 24px" }}
+            onKeyUp={(event) => event.keyCode === 13 && saveComment()}
+          >
+            <i className="fa fa-pencil"></i> Write comment
+            <input type="text" onChange={writing} />
+            <Button onClick={saveComment}>
+              <i className="fa fa-send"></i>
+            </Button>
+          </div>
+          <>
+            {comments.length ? (
+              <>
+                {comments.map((comment) => (
+                  <Comment
+                    key={comment.id}
+                    comment={comment}
+                    users={users}
+                    deleteComment={deleteComment}
+                  />
+                ))}
+              </>
+            ) : (
+              <></>
+            )}
+          </>
         </>
-    )
-}
+      )}
+    </>
+  );
+};
 
 export { Comments };
