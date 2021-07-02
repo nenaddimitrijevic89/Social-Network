fd8115ae2dd406ed05d0550fb82bafaafc705581
Nenad <nenaddimitrijevic89@gmail.com>
2021-07-02 23:26:06 +0200

    WIP: Fix post errors

--- src/view/components/Feed/Feed.js
+++ src/view/components/Feed/Feed.js
@@ -56,6 +56,7 @@ const Feed = () => {
   };
 
   const uploadImage = () => {
+    console.log(src)
     setIsLoading(true);
     postService.createImagePost(src).then(() => {
       postService
@@ -69,7 +70,6 @@ const Feed = () => {
   };
 
   const savePost = () => {
-    console.log(type)
     setIsLoading(true);
     postService.createPost(type, src).then(() => {
       postService
